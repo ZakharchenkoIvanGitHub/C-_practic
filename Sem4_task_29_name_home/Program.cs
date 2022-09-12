de01fbbeccd3bac06c17735197dd103415e69045
Inav Zakharchenko <zakharka2000@ya.ru>
2022-09-12 21:18:24 +0300

    добавил задачу 29 c именами

---
+++ Sem4_task_29_name_home/Program.cs
@@ -0,0 +1,36 @@
+﻿//==========================================
+//#29* Написать программу которая из имен через запятую
+//выберет случайное имя и выведет в терминал
+//==========================================
+
+string ReadData(string greeting = "Введите данные")
+//Получает строку заголовок
+//Выдает введенные данные
+{
+    //Выводим сообщение
+    Console.WriteLine(greeting);
+    //считываем строку
+
+    return Console.ReadLine() ?? "0";
+}
+
+//Парсит строку, возвращает массив подстрок
+string[] Parser(string str)
+{
+    return str.Split(',');
+}
+
+//Выбирает случайное имя из массива
+string GenRandName(string[] strArray)
+{
+    Random ren = new Random();
+    return strArray[ren.Next(0, strArray.Length)];
+}
+
+//выводит строку на печать
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+PrintResult("За пивом бежит "+GenRandName(Parser(ReadData("Введите имена через запятую"))));
